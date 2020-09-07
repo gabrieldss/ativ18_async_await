dbb2e18febf345a8d21f103156d9bad72c1c9a31
Gabriel do Santos Souza <gabriel.souza@forlogic.org>
2020-09-06 21:25:36 -0300

    basic-server refactored and adjustment in client

--- basic-server.js
+++ basic-server.js
@@ -1,24 +1,26 @@
-
+//comand for test --> node .\basic-server.js
 const fs = require('fs');
 const express = require('express'); 
-
+const axios = require('axios').default;
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('some request');
-
-    // leitura async libera o event loop para processar outras requisicoes
-    fs.readFile('./html/basic.html', 'utf-8', (err, data) => {
-        if (err) {
-            res.send(err);
-            return;
-        } 
-        res.send(data);
-    });
-});
+(async () => {
+    const res = await Promise.all([
+        app.get('/', (req, res) => {
+            fs.readFile('./html/basic.html', 'utf-8', (err, data) => {
+                if (err) {
+                    res.send(err);
+                    return;
+                }    
+                res.send(data);
+            })
+        })
+    ]);
+})()
 
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost:3000`);
 });
 
 console.log('End of the script');
+
--- client.js
+++ client.js
@@ -1,23 +1,24 @@
+//comand for test --> npm run server .\client.js
 const axios = require('axios').default;
 
 (async () => {
-    
-axios.post('http://localhost:3000/books/3', {
-    ID: 3,
-    name: 'My Boook',
-    author: 'Me and only me'
-}),
+    const res = await Promise.all([
+        axios.post('http://localhost:3000/books/3', {
+            ID: 3,
+            name: 'My Boook',
+            author: 'Me and only me'
+        }),
 
-axios.post('http://localhost:3000/books/4', {
-    ID: 4,
-    name: 'My New Boook',
-    author: 'Me and only me, not more'
-}),
+        axios.post('http://localhost:3000/books/4', {
+            ID: 4,
+            name: 'My New Boook',
+            author: 'Me and only me, not more'
+        }),
 
-axios.get('http://localhost:3000/books'),
+        axios.get('http://localhost:3000/books'),
 
-axios.get('http://localhost:3000/log')
-
+        axios.get('http://localhost:3000/log')
+    ])
 }).then((res) => {
     console.log(res.data);
 })
